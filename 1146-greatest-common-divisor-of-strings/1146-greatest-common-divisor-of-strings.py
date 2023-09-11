283615a8dc55843ce34ca2c175ccb5d714b1286e
plintAn <124107186+plintAn@users.noreply.github.com>
2023-09-11 22:57:22 +0900

    Time: 38 ms (75.27%), Space: 16.3 MB (73.98%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        def gcd(a, b): #유클리드
+            while b:
+                a, b = b, a % b
+            return a
+
+        return str1[:gcd(len(str1), len(str2))]
