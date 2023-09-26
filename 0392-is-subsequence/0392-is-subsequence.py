4eded9a1ab3bf4740112f80cb388edffda0e4b76
plintAn <124107186+plintAn@users.noreply.github.com>
2023-09-26 21:43:04 +0900

    Time: 37 ms (73.53%), Space: 16.3 MB (43.64%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,19 +1,18 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
 
-        i = 0  # s의 인덱스
-        j = 0  # t의 인덱스
+        s_arr = list(s)
+        t_arr = list(t)
 
-        while i < len(s) and j < len(t):
-            # s의 i 번째 문자와 t의 j 번째 문자가 같으면
-            if s[i] == t[j]:
-                # s의 인덱스와 t의 인덱스를 모두 1 증가시킵니다.
+        i = 0
+        j = 0
+
+        while i < len(s_arr) and j < len(t_arr):
+            if s_arr[i] == t_arr[j]:
                 i += 1
                 j += 1
             else:
-                # s의 i 번째 문자와 t의 j 번째 문자가 다르면
-                # t의 인덱스를 1 증가시킵니다.
                 j += 1
 
-        # s의 인덱스가 s의 길이와 같으면 s는 t의 하위 시퀀스입니다.
-        return i == len(s)
+        return i == len(s_arr)
+
